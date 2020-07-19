4d7f0edfe8d5d1e92cb7a8770676efcc056633ee
Brian Matheus <brianmatheus@gmail.com>
2020-07-19 13:58:24 -0500

    Added joke CRUD endpoints
    Minor configuration changes

--- joke_test/migrations/Version20200719142329.php
+++ joke_test/migrations/Version20200719142329.php
@@ -6,9 +6,11 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Symfony\Component\Finder\Finder;
 
 final class Version20200719142329 extends AbstractMigration
 {
+
     public function getDescription() : string
     {
         return 'Initial Migration';
@@ -16,11 +18,50 @@ final class Version20200719142329 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $this->addSql('CREATE TABLE joke (id INT AUTO_INCREMENT NOT NULL, content VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;');
+        $this->addSql('CREATE TABLE joke (id INTEGER PRIMARY KEY, content VARCHAR(255) NOT NULL);');
+        $this->seedData();
     }
 
     public function down(Schema $schema) : void
     {
         $this->addSql('DROP TABLE joke');
+    }
+
+    private function seedData() : void
+    {
+        $this->addSql($this->seedInsertStatement());
+    }
+
+    private function seedInsertStatement() : string
+    {
+        $values = implode(",", $this->prepareDataForStatement());
+        return "INSERT INTO joke (id, content) VALUES {$values}";
+    }
+
+    private function prepareDataForStatement() : array
+    {
+        return array_map(function($row){
+          return "(".$row.")";
+        }, $this->parseCSVData());
+    }
+
+    private function parseCSVData() : array
+    {
+        $finder = new Finder;
+        $finder->in('/srv/app/var')->files()->name('reddit-cleanjokes.csv');
+        $rows = [];
+
+        foreach ($finder as $file) { $csv = $file; }
+
+        if (($handle = fopen($csv->getRealPath(), "r")) !== FALSE) {
+            $i = 0;
+            while (($data = fgetcsv($handle, null, PHP_EOL)) !== FALSE) {
+                $i++;
+                if ($i == 1) { continue; }
+                $rows[] = $data[0];
+            }
+            fclose($handle);
+        }
+        return $rows;
     }
 }
--- joke_test/src/Controller/JokeController.php
+++ joke_test/src/Controller/JokeController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Joke;
 
 class JokeController extends AbstractController
 {
     /**
-     * @Route("/joke", name="joke")
+     * @Route("/joke", name="joke", methods="GET")
      */
     public function index()
     {
@@ -18,40 +20,82 @@ class JokeController extends AbstractController
     }
 
     /**
-     * @Route("/joke/{id}", name="joke_show")
+     * @Route("/joke/random", name="joke_random", methods="GET")
      */
-    public function show(int $id)
+    public function random()
     {
+        $randomJoke = $this->getDoctrine()->getRepository(Joke::class)->random();
+
         return $this->json([
-          'data' => $id
+          'data' => $randomJoke
         ]);
     }
 
     /**
-     * @Route("/joke/random", name="joke_random")
+     * @Route("/joke/{id}", name="joke_show", methods="GET")
      */
-    public function random()
+    public function show(int $id)
     {
+        $joke = $this->getDoctrine()
+          ->getRepository(Joke::class)
+          ->find($id);
+
         return $this->json([
-          'data' => $data
-        ], 200);
+          'data' => $joke
+        ]);
     }
 
     /**
-     * @Route("/joke/new", name="joke_new")
+     * @Route("/joke", name="joke_new", methods="POST")
      */
     public function new(array $data)
     {
+        $joke = new Joke;
+        $joke->setContent($request->query->get('content'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($joke);
+        $em->flush();
+
         return $this->json([
           'data' => $data
         ], 200);
     }
 
     /**
-     * @Route("/joke/delete", name="joke_delete")
+     * @Route("/joke/{id}", name="joke_edit", methods="POST")
+     */
+    public function edit(Request $request, int $id)
+    {
+        $joke = $joke = $this->getDoctrine()
+          ->getRepository(Joke::class)
+          ->find($id);
+        $joke->setContent($request->query->get('content'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($joke);
+        $em->flush();
+
+        return $this->json([
+          'data' => $joke
+        ], 200);
+    }
+
+    /**
+     * @Route("/joke/{id}", name="joke_delete", methods="DELETE")
      */
     public function delete(int $id)
     {
+        $joke = $joke = $this->getDoctrine()
+          ->getRepository(Joke::class)
+          ->find($id);
 
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($joke);
+        $em->flush();
+
+        return $this->json([
+            'deleted' => $id
+        ]);
     }
 }
